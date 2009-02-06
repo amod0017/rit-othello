656c350736d20066d9040c2b8d6c1ac9d9894a42
allultima1 <allultima1@083bf514-dddc-11dd-bce9-bf72c0239d3a>
2009-02-06 18:22:47 +0000

    finished alpha-beta and added mtd(f)

--- trunk/Othello/src/OthelloAlphaBeta.java
+++ trunk/Othello/src/OthelloAlphaBeta.java
@@ -1,4 +1,4 @@
-import java.util.HashMap;
+import java.util.*;
 
 /**
  * 
@@ -9,10 +9,17 @@ import java.util.HashMap;
  * @author Nicholas Ver Hoeve
  */
 public class OthelloAlphaBeta {
+	Map<BoardHash, Window> transpositionTable;
+	int minDepthToStore;
+	
+	/*
+	 * This class bundles alpha and beta so the range can be stored in a map
+	 */
 	public static class Window {
 		int alpha;
 		int beta;
 		
+		Window() {}
 		Window(int alpha, int beta) {
 			this.alpha = alpha;
 			this.beta = beta;
@@ -36,11 +43,15 @@ public class OthelloAlphaBeta {
 		}
 	};
 	
-	HashMap<BoardHash, Window> transpositionTable;
+	OthelloAlphaBeta(int minDepthToStore) {
+		this.minDepthToStore = minDepthToStore;
+		transpositionTable = new HashMap<BoardHash, Window>();
+	}
 	
 	public int AlphaBetaSearch(OthelloBitBoard position, int alpha, int beta, 
 			int turn, int depth) {
-		Window storedWindow = transpositionTable.get(new BoardHash(position, depth));
+		BoardHash hash = new BoardHash(position, depth);
+		Window storedWindow = transpositionTable.get(hash);
 		
 		if (storedWindow != null)
 		{
@@ -51,11 +62,14 @@ public class OthelloAlphaBeta {
 			if (storedWindow.beta <= alpha) {
 				return storedWindow.alpha;
 			}
+		} else {
+			storedWindow = new Window(); // go ahead and allocate
 		}
 		
 		//align windows
 		alpha = Math.min(alpha, storedWindow.alpha);
 		beta = Math.min(beta, storedWindow.beta);
+		int bestScore = 0x80000000; // initialized to 'very low'
 		
 		for (long likelyMoves = position.generateLikelyMoves(turn);
 				likelyMoves != 0;
@@ -70,11 +84,73 @@ public class OthelloAlphaBeta {
 			
 			OthelloBitBoard newPosition = position.copyAndMakeMove(moveX, moveY, turn);
 			
-			int newScore = AlphaBetaSearch(newPosition, -beta, -alpha, turn ^ 1, depth);
+			int newScore;
+			if (depth <= 1) { // base case
+				newScore = evaluateLeaf(newPosition, turn);
+			} else {//recurse
+				if (depth < minDepthToStore) {
+					newScore = AlphaBetaNoTable(newPosition, -beta, 
+							-Math.max(alpha, bestScore), turn ^ 1, depth - 1);
+				} else {
+					newScore = AlphaBetaSearch(newPosition, -beta, 
+							-Math.max(alpha, bestScore), turn ^ 1, depth - 1);
+				}
+				
+			}
 			
+			if (newScore > bestScore) {
+				bestScore = newScore;
+			}	
 		}
 		
-		return 0;
+		if (bestScore <= alpha) { // if fail low
+			storedWindow.beta = bestScore; // we know that at BEST the score is this bad
+		} else if (bestScore >= beta) { // if fail high
+			storedWindow.alpha = bestScore; // we know that at WORST the score is this good
+		} else {
+			storedWindow.alpha = storedWindow.beta = bestScore; // store exact value
+		}
+		
+		transpositionTable.put(hash, storedWindow); // store results for future lookup
+		
+		return bestScore;
+	}
+	
+	public int AlphaBetaNoTable(OthelloBitBoard position, int alpha, int beta, 
+			int turn, int depth) {
+		int bestScore = 0x80000000; // initialized to 'very low'
+		
+		for (long likelyMoves = position.generateLikelyMoves(turn);
+				likelyMoves != 0;
+				likelyMoves &= (likelyMoves - 1)) {
+			int movePos = BitUtil.ulog2(BitUtil.lowSetBit(likelyMoves));
+			int moveX = OthelloBitBoard.xyTox(movePos);
+			int moveY = OthelloBitBoard.xyToy(movePos);
+			
+			if (!position.moveIsLegal(moveX, moveY, turn)) {
+				continue;
+			}
+			
+			OthelloBitBoard newPosition = position.copyAndMakeMove(moveX, moveY, turn);
+			
+			int newScore;
+			if (depth <= 1) { // base case
+				newScore = evaluateLeaf(newPosition, turn);
+			} else {//recurse
+				newScore = AlphaBetaNoTable(newPosition, -beta, 
+						-Math.max(alpha, bestScore), turn ^ 1, depth - 1);
+			}
+			
+			if (newScore > bestScore) {
+				bestScore = newScore;
+			}	
+		}
+		
+		return bestScore;
+	}
+	
+	private int evaluateLeaf(OthelloBitBoard position, int state) {
+		return (position.countPieces(state) - position.countPieces(state ^ 1)) << 4;
 	}
 	
 
---
+++ trunk/Othello/src/OthelloMTDf.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ * @author Nicholas Ver Hoeve
+ */
+public class OthelloMTDf extends OthelloAlphaBeta {
+
+	OthelloMTDf(int minDepthToStore) {
+		super(minDepthToStore);
+	}
+	
+	int searchMTDf(OthelloBitBoard position, int turn, int depth) {
+		return searchMTDf(position, 0, turn, depth);
+	}
+	
+	int searchMTDf(OthelloBitBoard position, int guess, int turn, int depth) {
+		int alpha = 0x80000000;
+		int beta  = 0x7FFFFFFF;
+		int nullWindow;
+		
+		do {
+			nullWindow = guess;
+			if (guess == alpha)
+			{
+				nullWindow = guess + 1;
+			}
+
+			guess = AlphaBetaSearch(position, 
+					nullWindow-1, nullWindow, turn, depth);
+			
+			if (guess < nullWindow) { // if it failed low
+				beta = guess;
+			} else { // it must have failed high
+				alpha = guess;
+			}
+		} while (alpha < beta);
+		
+		return guess;
+	}
+	
+	int iterativeMTDf(OthelloBitBoard position, int turn, int depth) {
+		int guess = 0;
+		for (int d = 0; d < depth; d += 2) {
+			guess = searchMTDf(position, guess, turn, d);
+		}
+		 
+		return guess;
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
